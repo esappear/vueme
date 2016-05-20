ca8b278414e06c963be670cc3f61daf187b535ef
yxp <624835942@qq.com>
2016-05-20 16:39:23 +0800

    add todo list

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 /**
  * Created by qingcheng on 16/5/20.
  */
-console.log('Hello world!');
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + ''));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {}, function (err) {
+        if(err){
+            console.log(err);
+            res.status(err.status).end();
+        }else {
+            console.log('Send!');
+        }
+    })
+});
+
+app.listen('8080', function (req, res) {
+    console.log('App build success!');
+});
