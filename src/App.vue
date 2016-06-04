231c57c1b95480f3003ae9239396a2a2b05c3601
yxp <624835942@qq.com>
2016-06-04 21:47:56 +0800

    添加header组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
+    <!--html5有默认的header,直接header组件会失效-->
+    <header-component></header-component>
     <div class="container">
-        <h1>{{msg}}</h1>
-        <p class="row">
-            <a v-link="'bar'" class="col-lg-6">Go to Bar</a>
-            <a v-link="'todo'" class="col-lg-6">Go to Todo</a>
-        </p>
-
         <router-view></router-view>
     </div>
 </template>
@@ -13,11 +9,15 @@
 
 </style>
 <script>
+    import Header from './components/header.vue'
     module.exports = {
         data: function () {
             return {
                 msg: 'Hello Vue'
             }
+        },
+        components: {
+            'header-component': Header
         }
     };
 </script>
---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <i class="glyphicon glyphicon-time"></i>
+                {{msg}}
+            </a>
+            <ul class="nav navbar-nav">
+                <li v-for="link in links"><a v-link="link.name">{{link.txt}}</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+<style>
+
+</style>
+<script>
+
+    module.exports = {
+        data: function (){
+            return {
+                msg: 'hello vue',
+                links :[
+                    {name: 'bar', txt: 'Go to Bar'},
+                    {name: 'todo', txt: 'Go to Todo'}
+                ]
+            }
+        }
+    }
+</script>
